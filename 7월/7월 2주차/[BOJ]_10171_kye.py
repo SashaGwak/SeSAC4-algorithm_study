9a34cd19c4f2e75d808688caf74ee45068622dbb
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-07-11 23:32:30 +0900

    [BOJ] 10171 cat - kye

---
+++ 7월/7월 2주차/[BOJ]_10171_kye.py
@@ -0,0 +1,18 @@
+def msg() :
+    return '\    /\\'       
+
+print(msg())
+
+def msg1() :
+    return " )  ( ')"
+
+print(msg1())
+
+def msg2() :
+    return "(  /  )"
+
+print(msg2())
+
+def msg3() :
+    return " \(__)|"
+print(msg3())
