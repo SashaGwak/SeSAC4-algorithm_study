cddedbe811426c67dff8ed5252d4c439c8b88f30
Ethanolll <108229203+Ethanolll@users.noreply.github.com>
2022-07-16 01:23:22 +0900

    [BOJ] 9498번 시험성적 - nature

---
+++ 7월/7월 2주차/[BOJ]_9498_nature.py
@@ -0,0 +1,16 @@
+# 9498 시험 성적
+
+# 답
+score = input()
+S = int(score)
+
+if 90 <= S <= 100 :
+    print("A")
+elif 80 <= S < 90 :
+    print("B")
+elif 70 <= S < 80 :
+    print("C")
+elif 60 <= S < 70 :
+    print("D")
+else :
+    print("F")
