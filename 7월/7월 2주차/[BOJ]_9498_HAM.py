bca6b845f914c32b1e5ca868f2dd53d505250fb3
hamayj <zmlzml0307@gmail.com>
2022-07-18 21:35:45 +0900

    [BOJ]_9498_HAM.py

---
+++ 7월/7월 2주차/[BOJ]_9498_HAM.py
@@ -0,0 +1,14 @@
+test = int(input())
+
+if 90<=test<=100 :
+    print("A")
+elif 80<=test<=89 :
+    print("B")
+elif 70<=test<=79 :
+    print("C")
+elif 60<=test<=69 :
+    print("D")
+else :
+    print("F")
+
+# elif가 들여쓰기 아님 주의
