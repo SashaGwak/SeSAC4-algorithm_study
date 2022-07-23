5672f46b64f99dbc7be9b5d3a7617a42f4d272e1
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-07-23 22:03:29 +0900

    [BOJ] 10809 알파벳 찾기 - kye

---
+++ 7월/7월 3주차/[BOJ]_10809_kye.py
@@ -0,0 +1,9 @@
+#각각의 알파벳에 대해서,
+#단어에 포함되어 있는 경우에는 처음 등장하는 위치를,
+#포함되어 있지 않은 경우에는 -1을 출력하는 프로그램을 작성하시오.
+#a가 처음 등장하는 위치, b가 처음 등장하는 위치, ... z가 처음 등장하는 위치를
+#공백으로 구분해서 출력한다.
+
+word = input("단어를 입력해주세요.")
+print(word.find("a", 0), word.find("b", 0), word.find("c", 0), word.find("d", 0), word.find("e", 0), word.find("f", 0), word.find("g", 0), word.find("h", 0),word.find("i", 0), word.find("j", 0), word.find("k", 0),word.find("l", 0), word.find("m", 0), word.find("n", 0), word.find("o", 0), word.find("p", 0),
+      word.find("q", 0), word.find("r", 0), word.find("s", 0), word.find("t", 0), word.find("u", 0), word.find("v", 0), word.find("w", 0), word.find("x", 0), word.find("y", 0), word.find("z", 0))
