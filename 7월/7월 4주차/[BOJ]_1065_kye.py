6a4341e019f18b73b1d266b38620421b4dcb4b85
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-07-31 00:44:18 +0900

    [BOJ] 1065 한수 - kye

---
+++ 7월/7월 4주차/[BOJ]_1065_kye.py
@@ -0,0 +1,11 @@
+num = int(input())
+
+a = 0
+for i in range(1, num+1):
+    num_list = list(map(int, str(i)))
+    if i < 100:
+        a += 1
+    elif num_list[0] - num_list[1] == num_list[1] - num_list[2]:
+        a += 1
+print(a)
+        
