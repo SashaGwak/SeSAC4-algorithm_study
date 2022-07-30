b00e74b60a49122a481fbadc362980279065f06c
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-07-30 22:46:47 +0900

    [BOJ] 2908 상수 - kye

--- 7월/7월 4주차/[BOJ]_2908_kye.py
+++ 7월/7월 4주차/[BOJ]_2908_kye.py
@@ -1,8 +1,5 @@
-
-#1. 런타임에러
-
-num1 = input()
-num2 = input()
+#1. for loop
+num1, num2 = map(str, input().split())
 reverse_num1 = ''
 reverse_num2 = ''
 
@@ -12,7 +9,36 @@ for i in num1 :
 for i in num2 :
     reverse_num2 = i + reverse_num2
 
+reverse_num1 = int(reverse_num1)
+reverse_num2 = int(reverse_num2)
+
+
 if reverse_num1 > reverse_num2 :
     print(reverse_num1)
-else:
+else :
     print(reverse_num2)
+
+#2. slice()로 문자열 순서 뒤집기
+a, b = map(str, input().split())
+
+a = int(a[::-1])
+b = int(b[::-1])
+
+if a > b :
+    print(a)
+else :
+    print(b)
+
+#3.reversed()
+a, b = map(str, input().split())
+reversed_a = "".join(reversed(a))
+reversed_b = "".join(reversed(b))
+
+reversed_a = int(reversed_a)
+reversed_b = int(reversed_b)
+
+
+if reversed_a > reversed_b :
+    print(reversed_a)
+else :
+    print(reversed_b)
