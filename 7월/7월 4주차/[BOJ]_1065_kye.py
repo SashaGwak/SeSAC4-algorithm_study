44bb1cf648d8288912ac2e1582d4ab1eec6f77d2
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-07-31 21:32:04 +0900

    [BOJ] 1065 한수 - kye

--- 7월/7월 4주차/[BOJ]_1065_kye.py
+++ 7월/7월 4주차/[BOJ]_1065_kye.py
@@ -3,11 +3,10 @@
 #출력: 첫째 줄에 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력한다.
 
 num = int(input())
-
 a = 0
 for i in range(1, num+1):
     num_list = list(map(int, str(i)))
-    if i < 100:   #한자릿수, 두자릿수는 한수에 포함된다. 비교대상이 없기 때문에. 
+    if i < 100:   #한자릿수, 두자릿수는 한수에 포함된다. 
         a += 1
     elif num_list[0] - num_list[1] == num_list[1] - num_list[2]:
         a += 1
