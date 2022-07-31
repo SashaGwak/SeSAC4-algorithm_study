0ef0b7175cd817e8d3d1fd3059f73e9206edd4d5
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-07-31 21:30:16 +0900

    BOJ] 10870 피보나치수5 - kye

--- 7월/7월 4주차/[BOJ]_10870_kye .py
+++ 7월/7월 4주차/[BOJ]_10870_kye .py
@@ -1,9 +1,13 @@
+#문제: n이 주어졌을 때, n번째 피보나치 수를 구하는 프로그램을 작성하시오.
+#입력: 첫째 줄에 n이 주어진다. n은 20보다 작거나 같은 자연수 또는 0이다.
+#출력; 첫째 줄에 n번째 피보나치 수를 출력한다.
+
 #for loop 
 n = int(input())
 fibo_nums = [0, 1]
 
-for i in range(2, n+1):
-    num = fibo_nums[i-1] + fibo_nums[i-2]
+for i in range(2, n+1):   
+    num = fibo_nums[i-1] + fibo_nums[i-2]  # Fn = Fn-1 + Fn-2 (n ≥ 2)
     fibo_nums.append(num)
 
 print(fibo_nums[n])
@@ -16,4 +20,3 @@ def fibonacci(n):
 
 n = int(input())
 print(fibonacci(n))
-
