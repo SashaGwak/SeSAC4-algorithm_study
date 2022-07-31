16b8b9ce4edc785b20bd745a82a053b10ac2eb4f
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-07-31 21:23:17 +0900

    Update [BOJ]_1065_kye.py

--- 7월/7월 4주차/[BOJ]_1065_kye.py
+++ 7월/7월 4주차/[BOJ]_1065_kye.py
@@ -1,11 +1,14 @@
+#문제: N이 주어졌을 때, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오. 
+#입력: 첫째 줄에 1,000보다 작거나 같은 자연수 N이 주어진다.
+#출력: 첫째 줄에 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력한다.
+
 num = int(input())
 
 a = 0
 for i in range(1, num+1):
     num_list = list(map(int, str(i)))
-    if i < 100:
+    if i < 100:   #한자릿수, 두자릿수는 한수에 포함된다. 비교대상이 없기 때문에. 
         a += 1
     elif num_list[0] - num_list[1] == num_list[1] - num_list[2]:
         a += 1
 print(a)
-        
