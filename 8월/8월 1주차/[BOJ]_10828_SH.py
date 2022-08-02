ca97bd14ccf7bb2d3308a6325712db610bb75a67
sihagwak <sasha.gwak@gmail.com>
2022-08-02 21:06:48 +0900

    [BOJ] 10828 스택 -SH

--- 8월/8월 1주차/[BOJ]_10828_SH.py
+++ 8월/8월 1주차/[BOJ]_10828_SH.py
@@ -15,22 +15,26 @@ for i in range(N):
 
     # 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력
     elif command[0] == 'pop':
-        # 스택에 들어있는 정수가 없는 경우 -1를 출력
+        # 스택에 들어있는 정수가 없는 경우 -1을 출력
         if len(stack) == 0:
             print(-1)
         else:
             print(stack.pop())
 
+    # 스택에 들어있는 정수의 개수를 출력
     elif command[0] == 'size': 
         print(len(stack))
     
+    # 스택이 비어있으면 1 아니면 0을 출력
     elif command[0] == 'empty': 
         if len(stack) == 0:
             print(1)
         else:
             print(0)
-        
+    
+    # 스택의 가장 위에 있는 정수를 출력
     elif command[0] == 'top': 
+        # 스택에 들어있는 정수가 없는 경우 -1을 출력
         if len(stack) == 0:
             print(-1)
         else:
