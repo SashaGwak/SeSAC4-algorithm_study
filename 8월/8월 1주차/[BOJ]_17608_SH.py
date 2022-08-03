769164f9aa52dcf3b733894759dcb470d423feb0
sihagwak <sasha.gwak@gmail.com>
2022-08-04 02:33:56 +0900

    [BOJ] 17608 막대기 - SH

--- 8월/8월 1주차/[BOJ]_17608_SH.py
+++ 8월/8월 1주차/[BOJ]_17608_SH.py
@@ -16,6 +16,7 @@ max = heights[-1]
 count = 1
 # 끝 막대기부터 확인
 for i in range(-1, -len(heights) - 1 , -1):
+# for i in reversed(hegihts) 이런식으로 해줘도 더 간단할 듯 ! 
     # 만약 끝막대기보다 길이 긴 경우 count 1씩 커짐
     if heights[i] > max:
         count += 1
