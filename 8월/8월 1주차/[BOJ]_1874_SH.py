85bc74575134c833094627ba130b11b701e7e740
sihagwak <sasha.gwak@gmail.com>
2022-08-06 02:46:51 +0900

    [BOJ] 1874 스택 수열 -SH

--- 8월/8월 1주차/[BOJ]_1874_SH.py
+++ 8월/8월 1주차/[BOJ]_1874_SH.py
@@ -1,10 +1,30 @@
 # 스택 수열(1874)
 import sys
 n = int(input())
-stack = []
 
-for i in range( n ):
+count = 1
+stack = []   # stack 담을 리스트 
+result = []  # 출력값 담을 리스트
+flag = True  
+
+for _ in range(n):
     num = int(sys.stdin.readline())
-    stack.append(num)
 
-print(stack)
+    # 입력되는 값이 count(1부터 시작~) 보다 같거나 크다면 append
+    while count <= num:
+        stack.append(count)
+        # append할때마다 + 출력할 수 있도록 추가
+        result.append('+')
+        count += 1
+
+    if stack[-1] == num:
+        stack.pop()
+        result.append('-')
+    else:
+        flag = False 
+
+if not flag: 
+    print('NO')
+else:
+    for i in result:
+        print(i)
