5497497dcd14e0561acbc7f97e99c16e48d80e33
sihagwak <sasha.gwak@gmail.com>
2022-08-11 19:22:24 +0900

    [BOJ] 1406 에디터 - SH

--- 8월/8월 2주차/[BOJ]_1406_SH.py
+++ 8월/8월 2주차/[BOJ]_1406_SH.py
@@ -1,17 +1,29 @@
 # 에디터(1406)
 import sys
-text = list(sys.stdin.readline())
+text = list(sys.stdin.readline().strip())
 M = int(input())
+cursor = len(text)
 
+# 에디터 구현 부분
 for _ in range(M): 
     command = sys.stdin.readline().split()
 
+    if command[0] =='L':
+        if cursor > 0:
+            cursor -= 1 
+
+    if command[0] == 'D':
+        if cursor < len(text):
+            cursor += 1
+
     if command[0] == 'P' : 
-        text.append(command[1])
+        text.insert( cursor, command[1])
+        cursor += 1
+
+    if command[0] == 'B' : 
+        text.pop(cursor)
 
 
+# 결과 출력 부분
 for t in text:
-    if t == '\n': 
-        pass
-    else:
         print(t, end='')
