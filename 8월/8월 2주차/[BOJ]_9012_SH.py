f9e1a88683c27d14e46672ab4d01a9642168b21c
sihagwak <sasha.gwak@gmail.com>
2022-08-11 17:58:49 +0900

    [BOJ] 9012 괄호 - SH

--- 8월/8월 2주차/[BOJ]_9012_SH.py
+++ 8월/8월 2주차/[BOJ]_9012_SH.py
@@ -1,8 +1,20 @@
 # 괄호(9012)
-import sys
 T = int(input())
-test = []
 
 for _ in range(T):
-    test.append(sys.stdin.readline())
-
+    stack = []
+    a = input()
+    for i in a:
+        if i == '(':
+            stack.append(i)
+        elif i == ')':
+            if stack: # 스택에 괄호가 있을 경우 pop
+                stack.pop()
+            else: #  스택에 괄호가 없을경우 NO(')'만 남아있는 경우)
+                print("NO")
+                break
+    else: # break문으로 끊기지 않고 수행됬을경우 수행한다('('만 남아있는 경우)
+        if not stack: # break문으로 안끊기고 스택이 비어있다면 괄호가 다 맞는거다.
+            print("YES")
+        else: # break안 걸렸더라도 스택에 괄호가 들어있다면 NO이다.
+            print("NO")
