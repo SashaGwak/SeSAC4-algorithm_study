f3e09f167bc2d72f53b080c22e78c9b9636f0621
sjumkii <99640300+sjumkii@users.noreply.github.com>
2022-08-15 00:21:36 +0900

    [BOJ] 1259 팰린드롬수-soo

--- 8월/8월 2주차/[BOJ]_1259_nature.py
+++ 8월/8월 2주차/[BOJ]_1259_nature.py
@@ -1,10 +1,8 @@
-# 1259 팰린드롬 수
+num = input()
 
-while(True): 
-    a = input() # ex) radar
-    if a == a[::-1]: # ex) radar == radar
-        print("yes")
-    elif a == "0": # 0이면 출력 x
-        break
-    else: 
-        print("no")
+while num != '0':
+    if num[::-1] == num:
+        print('yes')
+    else:
+        print('no')
+    num = input()
