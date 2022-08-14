93f5a8d9e14a845116e8348c297a467dda25aa5a
sihagwak <sasha.gwak@gmail.com>
2022-08-15 02:43:16 +0900

    [BOJ] 10816 숫자카드 2-SH

--- 8월/8월 2주차/[BOJ]_10816_SH.py
+++ 8월/8월 2주차/[BOJ]_10816_SH.py
@@ -9,3 +9,36 @@
 # for i in check: 
 #     print(numbers.count(i), end=' ')
 # -> 시간 초과 뜬 답 
+import sys 
+input = sys.stdin.readline
+n = int(input())
+numbers = list(map(int,input().split()))
+m = int(input())
+check = list(map(int,input().split()))
+
+numbers.sort()
+
+def bs(num, bound): 
+    start, end = 0, n 
+    while(start < end): 
+        mid = (start + end) // 2 
+        # 동일한 수의 가장 낮은 인덱스 찾기
+        if bound == 0:
+            if numbers[mid] < num:
+                start = mid + 1
+            else: 
+                end = mid
+        # 동일한 수의 가장 높은 인덱스 찾기
+        else: 
+            if numbers[mid] <= num: 
+                start = mid + 1
+            else: 
+                end = mid 
+    return end 
+
+result = []
+for i in check: 
+    # 가장 높은 인덱스에서 가장 낮은 인덱스 빼주기 
+    # 예시 문제처럼 i=0 10일때 11-8 = 3
+    result.append(bs(i, 1) - bs(i, 0))
+print(*result)
