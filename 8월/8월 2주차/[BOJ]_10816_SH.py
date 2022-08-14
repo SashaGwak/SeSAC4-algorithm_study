c80e99034d0d68b28eae0efb5a58eee30a07305c
sihagwak <sasha.gwak@gmail.com>
2022-08-15 02:05:28 +0900

    [BOJ] 10816 숫자카드 2-SH

--- 8월/8월 2주차/[BOJ]_10816_SH.py
+++ 8월/8월 2주차/[BOJ]_10816_SH.py
@@ -1,9 +1,11 @@
 # 숫자 카드 2 (10816)
-N = int(input())
-numbers = list(map(int, input().split()))
-M = int(input())
-check = list(map(int, input().split()))
 
-for i in check: 
-    print(numbers.count(i), end=' ')
+# 첫번째 답안
+# N = int(input())
+# numbers = list(map(int, input().split()))
+# M = int(input())
+# check = list(map(int, input().split()))
+
+# for i in check: 
+#     print(numbers.count(i), end=' ')
 # -> 시간 초과 뜬 답 
