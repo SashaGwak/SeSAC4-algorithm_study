90047287e216ebc4857373ea3519c2628d8f4f7d
sihagwak <sasha.gwak@gmail.com>
2022-09-05 00:50:11 +0900

    [BOJ] 11650 좌표정렬하기 - SH

--- 9월/9월 1주차/[BOJ]_11650_SH.py
+++ 9월/9월 1주차/[BOJ]_11650_SH.py
@@ -1,3 +1,19 @@
 # 좌표 정렬하기(11650)
 import sys
-N = int(input())
+input = sys.stdin.readline
+
+n = int(input())
+
+# 좌표 모두 들어갈 리스트 만들기 
+array = []
+
+# 좌표 넣어주기 
+for i in range(n): 
+    [a, b] = map(int, input().split())
+    array.append([a, b]) 
+
+# 정렬 
+s_array = sorted(array)
+
+for i in range(n): 
+    print(s_array[i][0], s_array[i][1])
