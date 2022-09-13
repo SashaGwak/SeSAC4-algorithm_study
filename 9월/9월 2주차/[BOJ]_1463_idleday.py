e35f85a5217cfa3479b684823c7e9b4c0c4da531
idledaydreamer <idledaydreamer@korea.ac.kr>
2022-09-13 14:47:20 +0900

    [BOJ] 10844 쉬운 계단 수 - idleday

---
+++ 9월/9월 2주차/[BOJ]_1463_idleday.py
@@ -0,0 +1,20 @@
+# 1로만들기 (1463)
+
+
+n = int(input())
+
+# n+1개만큼의 0으로 이루어진 배열 만들기
+dp = [0 for _ in range(n+1)]
+
+# index == 입력(n)
+# index값 == 출력(연산최솟값)
+for i in range(2, n + 1):
+    dp[i] = dp[i - 1] + 1
+
+    if i % 2 == 0 and dp[i] > dp[i // 2] + 1:
+        dp[i] = dp[i // 2] + 1
+
+    if i % 3 == 0 and dp[i] > dp[i // 3] + 1:
+        dp[i] = dp[i // 3] + 1
+
+print(dp[n])
