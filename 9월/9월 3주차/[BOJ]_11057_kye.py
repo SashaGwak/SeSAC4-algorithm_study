00eb47b288d9bb6f59679e4162825b751f78c3c8
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-09-19 00:54:00 +0900

    [BOJ] 11057 오르막수 - kye

--- 9월/9월 3주차/[BOJ]_11057_kye.py
+++ 9월/9월 3주차/[BOJ]_11057_kye.py
@@ -1,9 +1,16 @@
-n = int(input())
-s = [[0] * 10 for i in range(1001)]
-for i in range(10):
-    s[1][i] = 1
-for i in range(2, 1001):
-    for j in range(10):
-        for k in range(j + 1):
-            s[i][j] += s[i - 1][k]
-print(sum(s[n]) % 10007)
+# 수 길이 N
+N = int(input())
+
+# 10개의 배열값을 갖는 DP 배열을 만든다
+DP = [[0]*10 for _ in range(N+1)]
+
+# # N = 1 -> dp[1] = 1을 10개 갖는 배열 지정
+DP[1] = [1] * 10
+
+for i in range(2, N+1): 
+    DP[i][0] = 1    # 끝자리 수 0 일 때 -> dp[1][0] = 0, dp[2][0] = 00, dp[3][0] = 000 이기 때문에 1 로 지정한다.
+    for k in range(10):
+        DP[i][k] = DP[i-1][k] + DP[i][k-1]
+        # ex) 길이가 2, 끝자리 수가 5인 DP[2][5] = DP[1][5] + DP[2][4]
+            
+print(sum(DP[N]) %10007)
