28af73373e28ccb941ca6c92b8d02668f344443f
sihagwak <sasha.gwak@gmail.com>
2022-09-19 01:45:38 +0900

    [BOJ] 11053 가장 긴 증가하는 부분 수열 - SH

--- 9월/9월 3주차/[BOJ]_11053_SH.py
+++ 9월/9월 3주차/[BOJ]_11053_SH.py
@@ -5,21 +5,22 @@ input = sys.stdin.readline
 N = int(input())
 A = list(map(int, input().split()))
 
-dp = [1] * N 
+dp = [0 for i in range(N)] 
 print('디피느 ㄴ머냐면', dp)
 
-for i in range(1, N): # 현재 인덱스 
+for i in range(N): # 현재 인덱스 
     print('아이',i)
     for j in range(i): # 인전 리스트 인자들과 비교 
         print('제이', j)
         print('A[i]!!!', A[i])
         print('A[j]!!!', A[j])
-        if A[i] > A[j]: # 뒤에나오는게 더 크면
-            ㅔ
-            dp[i] = max(dp[i], dp[j]+1)
+        if A[i] > A[j] and dp[i] < dp[j]: # 현재 인덱스가 앞에보다 크고, 전의 
+            print('여기가 더하기 전', dp)
+            dp[i] = dp[j]
             print('디피아잇', dp[i])
-            print('디피제에에에에에에에에이', dp[j] + 1)
+            print('디피제에에에에에에에에이', dp[j])
 
-
+        dp[i] += 1
+        print('여기가 더하기 후후후후후후후ㅜ불면은 ', dp)
 
 print(max(dp))
