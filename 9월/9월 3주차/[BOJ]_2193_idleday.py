5570fe6038b671a72b926544661d2bd4affc3a0d
idledaydreamer <idledaydreamer@korea.ac.kr>
2022-09-19 13:56:11 +0900

    [BOJ] 2193 이친수 -idleday

--- 9월/9월 3주차/[BOJ]_2193_idleday.py
+++ 9월/9월 3주차/[BOJ]_2193_idleday.py
@@ -6,6 +6,7 @@ n = int(input())
 # dp는 n자리수 배열 (n은 1이상 90이하)
 dp = [0] * (91)
 
+
 # n=1일때 1
 dp[1] = 1
 
@@ -22,5 +23,6 @@ dp[4] = 3
 # 점화식
 for i in range(2, n+1):
     dp[i] = dp[i-2] + dp[i-1]
+
 
 print(dp[n])
