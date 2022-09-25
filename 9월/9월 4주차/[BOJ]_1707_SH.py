891665f7efde1e52ab7e4759707c5ac94997e208
sihagwak <sasha.gwak@gmail.com>
2022-09-25 15:18:29 +0900

    [BOJ] 1707 이분그래프 - SH

--- 9월/9월 4주차/[BOJ]_1707_SH.py
+++ 9월/9월 4주차/[BOJ]_1707_SH.py
@@ -1,19 +1,23 @@
 # 이분그래프(1707)
 import sys 
+from collections import deque
 input = sys.stdin.readline
 
-def dfs(start, group):
-    visited[start] = group # 해당 정점의 group 설정(1,-1)
+# bfs
+def bfs(start, group):
+    queue = deque([start])  # 시작 정점 값을 큐에 담는다.
+    visited[start] = group  # 시작 정점 그룹을 설정
 
-    for i in graph[start]:
-        if not visited[i]: # 만약 방문하지 않았다면
-            a = dfs(i, -group) # 그룹값을 -1로 주어 dfs를 돈다.
-            if not a: # 만약 a가 false가 나왔다면
-                return False 
-        elif visited[i] == visited[start]:  # 만약 현재 정점과 연결된 정점의 그룹값이 같다면
-            return False  # False를 리턴
-    return True  # 그외의 경우는 True를 리턴
-
+    while queue: 
+        x = queue.popleft()  # 큐의 맨앞 원소를 빼낸다.
+        for i in graph[x]:  # 해당 정점에서 갈 수 있는 하위 정점들을 돈다.
+            if not visited[i]:  # 만약 그 정점들을 아직 방문하지 않았다면
+                queue.append(i)  # 그 정점들을 추가하고
+                visited[i] = -1 * visited[x]  # 상위 정점과 다른 그룹으로 편성
+            # 만약 정점들을 이미 방문했었는데 같은 그룹이라면
+            elif visited[i] == visited[x]:  
+                return False  # False를 바로 리턴
+    return True  # 위의 조건에 걸리지 않았다면 True를 리턴
 
 
 # T 테스트 케이스 개수 
@@ -30,10 +34,10 @@ for _ in range(T):
         graph[node1].append(node2)
         graph[node2].append(node1)
 
-    for i in range(1, V+1): 
-        if not visited[i]:  # 방문한 정점이 아니면, dfs 수행
-            result = dfs(i, 1)
-            if not result:  # 만약 result가 False가 나왔다면 더이상 수행할 필요가 없으므로
-                break 
+    for i in range(1, V + 1):
+        if not visited[i]:  # 방문한 정점이 아니면, bfs 수행
+            result = bfs(i, 1)
+            if not result:
+                break
 
     print("YES" if result else "NO")
