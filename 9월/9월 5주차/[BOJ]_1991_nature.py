d0bd2da91ef867d61cbd2a6d03c540ffd72c1e53
Ethanolll <108229203+Ethanolll@users.noreply.github.com>
2022-10-03 12:36:31 +0900

    [BOJ] 1991 트리 순회 - nature

--- 9월/9월 5주차/[BOJ]_1991_nature.py
+++ 9월/9월 5주차/[BOJ]_1991_nature.py
@@ -9,9 +9,11 @@ for i in range(N):
     root, left, right = sys.stdin.readline().strip().split()
     tree[root] = [left, right]
 
+# 자식 노드가 없는 경우 .으로 표현 
+
 # root -> left -> right
 def preorder(root):
-    if root != ".":
+    if root != ".": # 자식 노드가 있으면!
         print(root, end="") # root
         preorder(tree[root][0]) #left
         preorder(tree[root][1]) #right
