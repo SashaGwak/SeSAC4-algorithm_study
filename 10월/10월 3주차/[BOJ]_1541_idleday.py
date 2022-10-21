473765db13fceab7ac315f2a8ea0f1e5c3a41e2d
idledaydreamer <idledaydreamer@korea.ac.kr>
2022-10-21 23:54:17 +0900

    [BOJ] 1541 잃어버린괄호 - idleday

---
+++ 10월/10월 3주차/[BOJ]_1541_idleday.py
@@ -0,0 +1,14 @@
+# 1541 잃어버린 괄호
+
+a = input().split('-')
+num = []
+for i in a:
+    cnt = 0
+    s = i.split('+')
+    for j in s:
+        cnt += int(j)
+    num.append(cnt)
+n = num[0]
+for i in range(1, len(num)):
+    n -= num[i]
+print(n)
