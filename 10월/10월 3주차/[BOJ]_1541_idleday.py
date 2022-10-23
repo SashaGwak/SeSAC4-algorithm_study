37344277305df3749f12de05c270e5a2f93c147c
idledaydreamer <idledaydreamer@korea.ac.kr>
2022-10-23 21:22:45 +0900

    [BOJ] 1541 잃어버린 괄호 - idleday

--- 10월/10월 3주차/[BOJ]_1541_idleday.py
+++ 10월/10월 3주차/[BOJ]_1541_idleday.py
@@ -1,14 +1,19 @@
 # 1541 잃어버린 괄호
 
-a = input().split('-')
-num = []
-for i in a:
-    cnt = 0
-    s = i.split('+')
-    for j in s:
-        cnt += int(j)
-    num.append(cnt)
-n = num[0]
-for i in range(1, len(num)):
-    n -= num[i]
-print(n)
+# 입력값에서 '-' 기준으로 문자열 분리
+s = input().split("-")
+
+# 모든 숫자의 총합을 담기 위한 변수 sum 선언
+sum = 0
+
+# 맨 처음 숫자부터 '-'가 나오기 전까지의 숫자들을 각각 분리하고 모두 더하기
+for i in s[0].split("+"):
+    sum += int(i)
+
+# '-'가 나온 이후의 숫자들을 각각 숫자로 분리하고 모두 빼기
+for i in s[1:]:
+    for j in i.split("+"):
+        sum -= int(j)
+
+# 결과 출력
+print(sum)
