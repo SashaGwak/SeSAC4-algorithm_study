a5453f34466259ababbefe3efc2c55fdfb401dc4
sihagwak <sasha.gwak@gmail.com>
2022-10-23 20:23:22 +0900

    [BOJ] 1541  잃어버린 괄호 -SH

--- 10월/10월 3주차/[BOJ]_1541_SH.py
+++ 10월/10월 3주차/[BOJ]_1541_SH.py
@@ -1,11 +1,17 @@
 # 잃어버린 괄호(1541)
+# 괄호를 쳐서 이 식의 값을 최소로 만들기
 arr = input().split('-')
-s = 0
+# -를 기준으로 괄호 치기 ex) 55 - (50+40)
 
+# 결과값 저장
+result = 0
+
+# +으로 나누어 더해주기(-앞의 부분)
 for i in arr[0].split('+'):
-    s += int(i)
+    result += int(i)
 for i in arr[1:]:
+    # - 뒤부분 -해주기
     for j in i.split('+'):
-        s -= int(j)
+        result -= int(j)
         
-print(s)
+print(result)
