943ef04ff80bfa9867139046ed39e601caf8047a
idledaydreamer <idledaydreamer@korea.ac.kr>
2022-11-06 13:22:26 +0900

    [BOJ] 7568 덩치 - idleday

--- 10월/10월 4주차/[BOJ]_7568_idleday.py
+++ 10월/10월 4주차/[BOJ]_7568_idleday.py
@@ -1,17 +1,24 @@
 # 7568 덩치
-# 완전탐색(Brute-Force Search)
+# 완전탐색(Brute-Force Search) : 모든 경우의 수 확인
 
-num_student = int(input())  # 모든 사람의 수
-student_list = []   # 각 사람의 몸무게와 키를 담을 리스트
+N = int(input())    # 전체 사람 수
+people = []         # 몸무게와 키를 담을 리스트
 
-for _ in range(num_student):
+# 입력값 받기
+for _ in range(N):
+    # 각 사람의 몸무게와 키를 입력받아 튜플 형태로 리스트에 삽입
     weight, height = map(int, input().split())
-    # 몸무게와 키를 튜플로 리스트에 삽입
-    student_list.append((weight, height))
+    people.append((weight, height))
 
-for i in student_list:  #
-    rank = 1            #
-    for j in student_list:              #
-        if i[0] < j[0] and i[1] < j[1]:  # 몸무게와 키 모두 더 큰 사람이 있는 경우
-            rank += 1                   # 덩치 등수 더해서 자기 등수 정하기
+# 덩치 순위 구하기
+for self in people:
+    # 덩치 등수 초기값은 1
+    rank = 1
+    # 리스트의 모든 사람(자신 포함)과 비교
+    for other in people:
+        # 자신보다 더 큰 덩치의 사람이 있을 경우
+        if self[0] < other[0] and self[1] < other[1]:  # (0: 몸무게, 1: 키)
+            # 덩치 등수 +1
+            rank += 1
+    # 자신의 최종 덩치 등수 출력
     print(rank, end=" ")
