a4bf048350cbde3946b537fc8a70ac8fa0b81fce
junghwaK <jeonghwa054@gmail.com>
2022-11-14 00:09:29 +0900

    [BOJ] 16173 점프왕 쩰리 - junghwaK

--- 11월/11월 2주차/[BOJ]_16173_junghwaK.py
+++ 11월/11월 2주차/[BOJ]_16173_junghwaK.py
@@ -1,19 +1,42 @@
 # 16173 점프왕 쩰리
 
+import sys
 from collections import deque
-N = int(input())
-board = [list(map(int, input().split())) for _ in range(N)]
-queue = deque()
-queue.append([0, 0])
-while queue:
-    x, y = queue.popleft()
-    if board[x][y] == 0:
-        continue
-    if x == N-1 and y == N-1:
-        print("HaruHaru")
-        exit()
-    if 0 <= x+board[x][y] < N and 0 <= y < N:
-        queue.append([x+board[x][y], y])
-    if 0 <= x < N and 0 <= y+board[x][y] < N:
-        queue.append([x, y+board[x][y]])
-print("Hing")
+
+
+def solution(N, graph):
+    visited = [[False] * N for _ in range(N)]
+
+    dx = [1, 0]
+    dy = [0, 1]
+
+    def dfs(x, y, visited):
+        queue = deque()
+        queue.append((x, y))
+
+        while queue:
+            x, y = queue.popleft()
+            if graph[x][y] == -1:
+                return 'HaruHaru'
+
+            jump = graph[x][y]  # 점프 값
+            for i in range(2):
+                nx = x + dx[i] * jump
+                ny = y + dy[i] * jump
+
+                if nx >= N or ny >= N:
+                    continue
+                if visited[nx][ny]:
+                    continue
+                else:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
+        return 'Hing'
+
+    print(dfs(0, 0, visited))
+
+
+if __name__ == "__main__":
+    N = int(sys.stdin.readline())
+    graph = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+    solution(N, graph)
